60cc6ebd3e3e5400585ce181f8c648c896d63225
John <75003086+ZYJLiu@users.noreply.github.com>
2024-08-12 04:40:59 -0500

    Remove alias config causing all Nextjs App directory paths to error (#8)
    * remove alias causing app dir routes to throw Error: Cannot read properties of null (reading 'useContext')
    
    * remove unused path import from nextjs.config

--- next.config.js
+++ next.config.js
@@ -6,7 +6,6 @@ const withBundleAnalyzer = require("@next/bundle-analyzer");
 const { i18n } = require("./next-i18next.config");
 const rewritesAndRedirectsJson = require("./rewrites-redirects.json");
 const { builder } = require("@builder.io/sdk");
-const path = require("path");
 
 const securityHeaders = [
   {
@@ -112,12 +111,6 @@ const moduleExports = () => {
         (rule) => rule.loader == "next-image-loader",
       );
       imageLoaderRule.exclude = /\.inline\.svg$/;
-
-      // Alias configuration
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        react: path.resolve(__dirname, "node_modules/react"),
-      };
 
       return config;
     },
