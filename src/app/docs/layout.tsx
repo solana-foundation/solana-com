b0826f951e7ab46cbda711f27f6560ea8d32ac82
Rodrigo Pombo <pombopombopombo@gmail.com>
2024-11-14 14:00:15 +0100

    Sidebar

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -3,13 +3,29 @@ import { DocsLayout } from "fumadocs-ui/layouts/docs";
 import type { ReactNode } from "react";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { Sidebar } from "fumadocs-ui/layouts/docs/sidebar";
+import { SidebarItems } from "fumadocs-ui/layouts/docs.client";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <>
       <Header containerClassName="container-docs" />
-      <div className="container-xl container-docs">
-        <DocsLayout tree={source.pageTree}>{children}</DocsLayout>
+      <div className="container-xl container-docs fumadocs">
+        <DocsLayout
+          tree={source.pageTree}
+          sidebar={{
+            component: (
+              <Sidebar
+                className="bg-transparent sticky overflow-auto overflow-x-hidden min-w-[var(--fd-sidebar-width)] scrollbar-thin"
+                style={{ top: 76, maxHeight: "calc(100vh - 76px)" }}
+              >
+                <SidebarItems />
+              </Sidebar>
+            ),
+          }}
+        >
+          {children}
+        </DocsLayout>
       </div>
       <Footer />
     </>
