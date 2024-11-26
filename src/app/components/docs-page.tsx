cda7e0921023924f17aff66f223501c6ce28b407
Rodrigo Pombo <pombopombopombo@gmail.com>
2024-11-26 14:43:55 +0100

    Add cookbook and rpc

---
+++ src/app/components/docs-page.tsx
@@ -0,0 +1,52 @@
+import {
+  DocsPage as FumaDocsPage,
+  DocsBody,
+  DocsTitle,
+  DocsPageProps,
+} from "fumadocs-ui/page";
+import GithubIcon from "@@/public/src/img/footer/github.inline.svg";
+import { ReactNode } from "react";
+
+export function DocsPage(props: {
+  children: ReactNode;
+  filePath: string;
+  toc: DocsPageProps["toc"];
+  hideTableOfContents?: boolean;
+  full?: boolean;
+  title: string;
+}) {
+  return (
+    // @ts-ignore
+    <FumaDocsPage
+      toc={props.toc}
+      full={props.full}
+      breadcrumb={{
+        enabled: true,
+        includeRoot: { url: "/docs" },
+        includeSeparator: true,
+      }}
+      tableOfContent={{
+        footer: <EditOnGithub path={props.filePath} />,
+        enabled: !props.hideTableOfContents,
+      }}
+    >
+      <DocsTitle className="text-fd-accent-foreground">{props.title}</DocsTitle>
+      <DocsBody>{props.children}</DocsBody>
+    </FumaDocsPage>
+  );
+}
+
+function EditOnGithub({ path }: { path: string }) {
+  const href = `https://github.com/solana-foundation/solana-com/blob/main/${path.startsWith("/") ? path.slice(1) : path}`;
+  return (
+    <a
+      href={href}
+      target="_blank"
+      rel="noreferrer noopener"
+      className="pt-2 flex items-center gap-2 text-sm text-fd-muted-foreground hover:text-fd-accent-foreground/80"
+    >
+      <GithubIcon width="18" height="18" />
+      <span>Edit page</span>
+    </a>
+  );
+}
---
+++ src/app/docs/rpc/[[...slug]]/page.tsx
@@ -0,0 +1,63 @@
+import { docsSource } from "@/app/source";
+import { notFound } from "next/navigation";
+
+import {
+  DocSideBySide,
+  DocLeftSide,
+  Parameter,
+  Field,
+  Values,
+  DocRightSide,
+} from "@/components/shared/MarkdownRenderer/DocSideBySide";
+import { DocsPage } from "@/app/components/docs-page";
+import { mdxComponents } from "@/app/mdx-components";
+
+const rpcMDXComponents = {
+  DocSideBySide,
+  DocLeftSide,
+  Parameter,
+  Field,
+  Values,
+  DocRightSide,
+};
+
+export default async function Page(props: {
+  params: Promise<{ slug?: string[] }>;
+}) {
+  const params = await props.params;
+  const slug = params.slug ? ["rpc", ...params.slug] : ["rpc"];
+  const page = docsSource.getPage(slug);
+  if (!page) notFound();
+  const MDX = page.data.body;
+
+  return (
+    // @ts-ignore
+    <DocsPage
+      toc={page.data.toc}
+      full={page.data.full}
+      title={page.data.title}
+      filePath={page.file.path}
+      hideTableOfContents={page.data.hideTableOfContents}
+    >
+      <MDX components={{ ...mdxComponents, ...rpcMDXComponents }} />
+    </DocsPage>
+  );
+}
+
+export async function generateStaticParams() {
+  return docsSource.generateParams();
+}
+
+export async function generateMetadata(props: {
+  params: Promise<{ slug?: string[] }>;
+}) {
+  const params = await props.params;
+  const slug = params.slug ? ["rpc", ...params.slug] : ["rpc"];
+  const page = docsSource.getPage(slug);
+  if (!page) notFound();
+
+  return {
+    title: page.data.title,
+    description: page.data.description,
+  };
+}
