a6f34b5eacd21657ad42e86b1bed5277e4ff7563
Rodrigo Pombo <pombopombopombo@gmail.com>
2024-11-27 14:50:38 +0100

    Add postcss-conditional-purge

--- postcss.config.js
+++ postcss.config.js
@@ -14,43 +14,43 @@ module.exports = {
         },
       },
     ],
-    // [
-    //   "@fullhuman/postcss-purgecss",
-    //   {
-    //     content: [
-    //       "./src/pages/**/*.{js,jsx,ts,tsx}",
-    //       "./src/components/**/*.{js,jsx,ts,tsx}",
-    //       "./src/app/**/*.{ts,tsx}",
-    //       "./node_modules/fumadocs-ui/dist/**/*.js",
-    //     ],
-    //     defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-    //     safelist: {
-    //       standard: ["html", "body"],
+    [
+      "./src/utils/postcss-conditional-purge",
+      {
+        ignore: /globals\.css$/,
+        content: [
+          "./src/pages/**/*.{js,jsx,ts,tsx}",
+          "./src/components/**/*.{js,jsx,ts,tsx}",
+          "./src/app/**/*.{ts,tsx}",
+          "./node_modules/fumadocs-ui/dist/**/*.js",
+        ],
+        defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+        safelist: {
+          standard: ["html", "body"],
 
-    //       greedy: [
-    //         // React-Bootstrap
-    //         /^navbar/,
-    //         /^modal/,
-    //         /^btn-close/,
-    //         /^ratio/,
-    //         /collapse/,
-    //         /^dropdown-/,
-    //         /^flex-/,
-    //         /^accordion-/,
-    //         /^spinner-/,
+          greedy: [
+            // React-Bootstrap
+            /^navbar/,
+            /^modal/,
+            /^btn-close/,
+            /^ratio/,
+            /collapse/,
+            /^dropdown-/,
+            /^flex-/,
+            /^accordion-/,
+            /^spinner-/,
 
-    //         // Third-party libraries
-    //         /^slick-/,
-    //         /^react-datetimerange-picker/,
-    //         /^react-calendar/,
-    //         /DocSearch-/,
+            // Third-party libraries
+            /^slick-/,
+            /^react-datetimerange-picker/,
+            /^react-calendar/,
+            /DocSearch-/,
 
-    //         // Tailwind
-    //         /tw-/,
-    //       ],
-    //     },
-    //   },
-    // ],
-    "autoprefixer",
+            // Tailwind
+            /tw-/,
+          ],
+        },
+      },
+    ],
   ],
 };
