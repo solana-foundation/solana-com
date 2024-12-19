b6d25dfa2337f2df09bd3db3ffe2f11cc28ade60
Catalin Rosu <me@catalin.red>
2024-12-19 21:49:31 +0200

    Reintroduce revalidate gradually (#133)

--- src/pages/developers/courses/index.tsx
+++ src/pages/developers/courses/index.tsx
@@ -34,7 +34,7 @@ export async function getStaticProps({ locale }) {
       records,
       featured,
     },
-    // revalidate: 60,
+    revalidate: 60,
   };
 }
 
--- src/pages/developers/guides/[...slug].tsx
+++ src/pages/developers/guides/[...slug].tsx
@@ -112,6 +112,6 @@ export async function getStaticProps({ params, locale }) {
       record,
       source,
     },
-    // revalidate: 60,
+    revalidate: 60,
   };
 }
