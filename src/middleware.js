91e235480a310e06d80a3567adf7dbda7c191d6b
Rodrigo Pombo <pombopombopombo@gmail.com>
2025-01-27 19:06:34 +0100

    Make slugs lowercase (#159)

--- src/middleware.js
+++ src/middleware.js
@@ -1,19 +1,18 @@
 import createMiddleware from "next-intl/middleware";
 import { routing } from "@/i18n/routing";
+import { NextResponse } from "next/server";
 
-export default createMiddleware(routing);
+const handleI18nRouting = createMiddleware(routing);
+
+export default async function middleware(req) {
+  if (req.nextUrl.pathname !== req.nextUrl.pathname.toLowerCase()) {
+    return NextResponse.redirect(
+      `${req.nextUrl.origin + req.nextUrl.pathname.toLowerCase()}`,
+    );
+  }
+  return handleI18nRouting(req);
+}
 
 export const config = {
   matcher: ["/((?!api|opengraph|_next|_vercel|.*\\..*).*)"],
 };
-
-// TODO add this back
-// export async function middleware(req) {
-//   if (req.nextUrl.pathname === req.nextUrl.pathname.toLowerCase()) {
-//     return NextResponse.next();
-//   }
-
-//   return NextResponse.redirect(
-//     `${req.nextUrl.origin + req.nextUrl.pathname.toLowerCase()}`,
-//   );
-// }
