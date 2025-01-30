35fb7815ddae805f924ac7555d7598b1ead3e6a5
Jacob Creech <82475023+jacobcreech@users.noreply.github.com>
2025-01-29 22:42:24 -0600

    chore: update stackexchange cta (#174)
    StackExchange CTA just being the title on the developer homepage is not great. Make it more actionable

--- src/app/[locale]/developers/developers.tsx
+++ src/app/[locale]/developers/developers.tsx
@@ -26,7 +26,7 @@ export function DevelopersPage({ latestChangelogVideo, guides, resources }) {
             href: "/docs/intro/quick-start",
           },
           secondary: {
-            label: "Stack Exchange",
+            label: "Get Support",
             href: "https://solana.stackexchange.com",
             icon: (
               <StackExchangeIcon width={16} height={20} fill="currentColor" />
