771b8da56d16783c0c3fa592fd47cfaf7cd92df1
Catalin Rosu <me@catalin.red>
2025-01-31 22:36:58 +0200

    Update index pages

--- src/pages/[locale]/breakpoint/index.js
+++ src/pages/[locale]/breakpoint/index.js
@@ -1,18 +1,15 @@
-import BreakpointPage, {
-  getStaticProps as getStaticPropsCatchAll,
-} from "./[...slug]";
 import { withLocales } from "@/i18n/routing";
+import dynamic from "next/dynamic";
 
-export default function Page(props) {
-  // We use an index page instead of an optional catch-all route
-  // because the optional catch-all route is 404ing in Vercel for localized routes
-  // similar to https://github.com/vercel/next.js/issues/62657
-  return <BreakpointPage {...props} />;
+const Page = dynamic(() => import("./[...slug]"), {
+  ssr: false,
+});
+
+export default function IndexPage(props) {
+  return <Page {...props} />;
 }
 
-export async function getStaticProps({ params }) {
-  return getStaticPropsCatchAll({ params });
-}
+export { getStaticProps } from "./[...slug]";
 
 export async function getStaticPaths() {
   return {
