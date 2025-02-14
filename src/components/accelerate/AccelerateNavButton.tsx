ea0566cce2971fc7c5c1de2ac9a67e398fa99adc
Georgios Diamantopoulos <georgiosd@gmail.com>
2025-02-14 11:17:45 +0200

    fix: navbar hover states

--- src/components/accelerate/AccelerateNavButton.tsx
+++ src/components/accelerate/AccelerateNavButton.tsx
@@ -1,4 +1,3 @@
-import classNames from "classnames";
 import { FC, ReactNode } from "react";
 import styles from "./AccelerateNavButton.module.scss";
 
@@ -6,17 +5,8 @@ export const AccelerateButton: FC<{
   children: ReactNode;
   variant: "none" | "scale" | "ship";
 }> = ({ children, variant = "none" }) => {
-  const isVariant = variant !== "none";
   return (
-    <button
-      data-variant={variant}
-      data-is-variant={isVariant}
-      className={classNames(
-        styles.root,
-        { [styles.some]: isVariant },
-        styles[variant],
-      )}
-    >
+    <button data-variant={variant} className={styles.root}>
       {children}
     </button>
   );
