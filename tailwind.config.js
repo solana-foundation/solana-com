2f559dff4c52bea56140a95f3a3852c62345bb57
Rodrigo Pombo <pombopombopombo@gmail.com>
2025-02-17 20:04:21 +0100

    Use codehike for syntax highlighting (#230)
    * Use codehike for syntax highlighting
    
    * Fix codeblock title

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,4 +11,18 @@ export default {
     // "./src/**/*.{js,ts,jsx,tsx,mdx}",
     // "./src/content/**/*.mdx",
   ],
+  theme: {
+    extend: {
+      colors: {
+        "ch-background": "hsl(var(--fd-secondary)/.5)", // editor.background
+        "ch-border": "hsl(var(--fd-border))", // editorGroup.border
+        "ch-selection": "var(--ch-20)", // editor.selectionBackground
+        "ch-tab-active-foreground": "var(--ch-4)", // tab.activeForeground
+        "ch-tab-inactive-foreground": "var(--ch-15)", // tab.inactiveForeground
+        "ch-line-number": "var(--ch-26)", // editorLineNumber.foreground
+        "ch-tabs-background": "hsl(var(--fd-muted))", // editorGroupHeader.tabsBackground
+        "ch-active-border": "var(--tw-prose-links)", // tab.activeBorderTop
+      },
+    },
+  },
 };
