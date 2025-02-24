53ca363e847dec295aa820ae5672e6ca3f36d44b
Rodrigo Pombo <pombopombopombo@gmail.com>
2025-02-24 12:39:27 +0100

    Use classnames instead of clsx

--- src/app/components/utils.tsx
+++ src/app/components/utils.tsx
@@ -1,6 +1,6 @@
-import { clsx, type ClassValue } from "clsx";
+import classNames, { ArgumentArray } from "classnames";
 import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+export function cn(...inputs: ArgumentArray) {
+  return twMerge(classNames(inputs));
 }
