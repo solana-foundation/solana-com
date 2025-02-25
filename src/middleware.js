3aa6c020c19eb608d0e9e9f606ade5ca5f8a77e7
Rodrigo Pombo <pombopombopombo@gmail.com>
2025-02-25 14:20:11 +0100

    Remove invalid search params

--- src/middleware.js
+++ src/middleware.js
@@ -1,6 +1,7 @@
 import createMiddleware from "next-intl/middleware";
 import { routing } from "@/i18n/routing";
 import { NextResponse } from "next/server";
+import { locales } from "@/i18n/config.cjs";
 
 const handleI18nRouting = createMiddleware(routing);
 
@@ -10,6 +11,17 @@ export default async function middleware(req) {
       `${req.nextUrl.origin + req.nextUrl.pathname.toLowerCase()}`,
     );
   }
+
+  const localeParam = req.nextUrl?.searchParams?.get("locale");
+  if (localeParam && !locales.includes(localeParam)) {
+    // An invalid locale search param means that the pages router was trying
+    // to do a soft navigation and matched the route pages/[locale]/[...slug]
+    // the right route will be resolved after the middleware adds the right locale prefix
+    // we can safely remove the locale and slug params to avoid poluting the URL
+    req.nextUrl.searchParams.delete("locale");
+    req.nextUrl.searchParams.delete("slug");
+  }
+
   return handleI18nRouting(req);
 }
 
