f39f7da43d520ba94ec40137fd0829cff8a066aa
John <johnzhengyuliu@gmail.com>
2025-02-25 19:20:18 -0600

    add inkeep searchbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,11 @@
 import Navbar from "react-bootstrap/Navbar";
 import { useRouter } from "@/hooks/useRouter";
 import { Link } from "../utils/Link";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import SolanaLogo from "../../public/src/img/logos-solana/logotype.inline.svg";
 import Moon from "../../public/src/img/icons/Moon.inline.svg";
 import Sun from "../../public/src/img/icons/Sun.inline.svg";
 import HeaderList from "./header/HeaderList";
-import { DocSearch } from "@docsearch/react";
 import { useTheme } from "@/themecontext";
 import { useTranslation } from "react-i18next";
 import DevelopersNav from "./developers/DevelopersNav/DevelopersNav";
@@ -19,11 +18,7 @@ const Header = ({ className = "", containerClassName = "" }) => {
   const { theme, toggleTheme, isThemePage } = useTheme();
   const { t } = useTranslation();
 
-  const [searchText, setSearchText] = useState("Search");
-
   useEffect(() => {
-    setSearchText(t("commands.search"));
-
     const navbar = document.getElementById("navbar");
     if (navbar) {
       if (isThemePage) {
@@ -62,22 +57,9 @@ const Header = ({ className = "", containerClassName = "" }) => {
                 <HeaderList />
               </Navbar.Collapse>
 
-              <DocSearch
-                // note: these values are safe to be published and are NOT secrets
-                apiKey={"011e01358301f5023b02da5db6af7f4d"}
-                appId={"FQ12ISJR4B"}
-                indexName={"solana-com"}
-                placeholder={searchText}
-                searchParameters={{
-                  facetFilters: [`language:${router?.locale || "en"}`],
-                }}
-                translations={{
-                  button: {
-                    buttonText: searchText,
-                    buttonAriaLabel: searchText,
-                  },
-                }}
-              />
+              <div className="ms-3">
+                <div id="inkeepSearchBar" />
+              </div>
 
               {isThemePage && (
                 <button
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,13 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import { InkeepScript } from "../app/components/inkeep-script";
 
 const Layout = ({ children }) => {
   return (
     <>
       <Header />
       <main>{children}</main>
+      <InkeepScript />
       <Footer />
     </>
   );
