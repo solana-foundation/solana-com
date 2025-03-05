2b8851010ee4d74bb6f2405cad7fbc6869864958
John <75003086+ZYJLiu@users.noreply.github.com>
2025-03-04 20:00:19 -0600

    docs: add feedback component using Inkeep API (#287)
    * add docs feedback component using inkeep api
    
    * refactor feedback component and inkeep server action
    
    * remove console.log
    
    * add log to debug vercel
    
    * await logging
    
    * close stream after logging
    
    * update feedback component, display response on button click
    
    * rename INFO to message in logs

--- src/app/[locale]/developers/cookbook/layout.tsx
+++ src/app/[locale]/developers/cookbook/layout.tsx
@@ -1,7 +1,7 @@
 import { cookbookSource } from "@/app/sources/cookbook";
 import type { ReactNode } from "react";
 import { DocsLayout } from "@/app/components/docs-layout";
-import { InkeepScript } from "@/app/components/inkeep-script";
+import { InkeepScript } from "@/app/components/inkeep/inkeep-script";
 export default async function Layout({
   children,
   params,
--- src/app/[locale]/developers/guides/[...slugs]/layout.tsx
+++ src/app/[locale]/developers/guides/[...slugs]/layout.tsx
@@ -1,7 +1,7 @@
 import { guidesSource } from "@/app/sources/guides";
 import type { ReactNode } from "react";
 import { DocsLayout } from "@/app/components/docs-layout";
-import { InkeepScript } from "@/app/components/inkeep-script";
+import { InkeepScript } from "@/app/components/inkeep/inkeep-script";
 
 export default async function Layout({
   children,
