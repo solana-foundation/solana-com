8344e0ab0326f0bfade7bc5226329add0a7b3427
sarah_inkeep <sarah.niemiec@inkeep.com>
2025-03-10 12:32:09 -0700

    Revert search bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,28 +5,27 @@ import { useRouter } from "@/hooks/useRouter";
 import { Link } from "../utils/Link";
 import {
   useEffect,
-  // useState
+  useState
 } from "react";
 import SolanaLogo from "../../public/src/img/logos-solana/logotype.inline.svg";
 import Moon from "../../public/src/img/icons/Moon.inline.svg";
 import Sun from "../../public/src/img/icons/Sun.inline.svg";
 import HeaderList from "./header/HeaderList";
-// import { DocSearch } from "@docsearch/react";
+import { DocSearch } from "@docsearch/react";
 import { useTheme } from "@/themecontext";
 import { useTranslation } from "react-i18next";
 import DevelopersNav from "./developers/DevelopersNav/DevelopersNav";
 import styles from "./Header.module.scss";
-import { InkeepSearchBar } from "../app/components/inkeep/inkeep-searchbar";
 
 const Header = ({ className = "", containerClassName = "" }) => {
   const router = useRouter();
   const { theme, toggleTheme, isThemePage } = useTheme();
   const { t } = useTranslation();
 
-  // const [searchText, setSearchText] = useState("Search");
+  const [searchText, setSearchText] = useState("Search");
 
   useEffect(() => {
-    // setSearchText(t("commands.search"));
+    setSearchText(t("commands.search"));
 
     const navbar = document.getElementById("navbar");
     if (navbar) {
@@ -38,7 +37,7 @@ const Header = ({ className = "", containerClassName = "" }) => {
       }
     }
   }, [
-    // t,
+    t,
     theme,
     isThemePage,
   ]);
@@ -69,9 +68,7 @@ const Header = ({ className = "", containerClassName = "" }) => {
               <Navbar.Collapse id="navbarCollapse">
                 <HeaderList />
               </Navbar.Collapse>
-              <InkeepSearchBar />
-
-              {/* <DocSearch
+              <DocSearch
                 // note: these values are safe to be published and are NOT secrets
                 apiKey={"011e01358301f5023b02da5db6af7f4d"}
                 appId={"FQ12ISJR4B"}
@@ -86,7 +83,7 @@ const Header = ({ className = "", containerClassName = "" }) => {
                     buttonAriaLabel: searchText,
                   },
                 }}
-              /> */}
+              />
 
               {isThemePage && (
                 <button
