e13114a0aa70ab963fe8d1748c00a69409508cb7
Catalin Rosu <me@catalin.red>
2025-03-10 09:36:46 +0200

    Simplify the default hero component

--- src/components/accelerate/AccelerateSimpleHero.jsx
+++ src/components/accelerate/AccelerateSimpleHero.jsx
@@ -1,5 +1,4 @@
 import AccelerateHero from "../../../assets/accelerate/hero.jpg";
-import AccelerateHeroA from "../../../assets/accelerate/hero-a.png";
 import Image from "next/image";
 import styles from "./AccelerateSimpleHero.module.scss";
 
@@ -10,13 +9,6 @@ const SimpleHero = ({ title }) => {
         src={AccelerateHero}
         alt=""
         className={styles["simplehero__img"]}
-        placeholder="blur"
-      />
-      <Image
-        src={AccelerateHeroA}
-        alt=""
-        className={styles["simplehero__img-blend"]}
-        placeholder="blur"
       />
       <div className="container my-8">
         <h1 className="fw-normal spacegrotesk">{title}</h1>
