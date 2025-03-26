04f91c40cca8e3bfc8a4d4b9ba8a9587f712a536
Hunter Simmons <hunterwilliamsimmons@gmail.com>
2025-03-25 22:06:45 -0600

    add use server

--- src/app/components/code/code.tsx
+++ src/app/components/code/code.tsx
@@ -1,4 +1,4 @@
-"use server"
+"use server";
 import { AnnotationHandler, Pre, RawCode, highlight } from "codehike/code";
 
 import { mark } from "./mark";
--- src/utils/mirror.ts
+++ src/utils/mirror.ts
@@ -1,3 +1,4 @@
+"use server";
 import { v4 as uuidv4 } from "uuid";
 import { cookies } from "next/headers";
 
