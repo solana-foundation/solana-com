471e3da04ff7d1e4ab9e7ca7e5386614a36cbbf0
vladan345 <vladanm087@gmail.com>
2025-04-01 12:59:46 +0200

    revert accelerate files from #372

--- src/pages/[locale]/accelerate/index.js
+++ src/pages/[locale]/accelerate/index.js
@@ -1,20 +1,17 @@
-import AcceleratePage from "./AcceleratePage";
+import AcceleratePage, {
+  getStaticProps as getStaticPropsCatchAll,
+} from "./[...slug]";
 import { withLocales } from "@/i18n/routing";
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 export default function Page(props) {
+  // We use an index page instead of an optional catch-all route
+  // because the optional catch-all route is 404ing in Vercel for localized routes
+  // similar to https://github.com/vercel/next.js/issues/62657
   return <AcceleratePage {...props} />;
 }
 
 export async function getStaticProps({ params }) {
-  const { locale } = params;
-
-  return {
-    props: {
-      params,
-      ...(await serverSideTranslations(locale, ["common"])),
-    },
-  };
+  return getStaticPropsCatchAll({ params });
 }
 
 export async function getStaticPaths() {
