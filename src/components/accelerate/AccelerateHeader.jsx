950f1f84fed48d26e8cf0b5e11260ec4386597fe
jon wong <j@jnwng.com>
2025-04-23 07:26:35 -0400

    Updating link

--- src/components/accelerate/AccelerateHeader.jsx
+++ src/components/accelerate/AccelerateHeader.jsx
@@ -50,7 +50,13 @@ const Header = () => {
             <a href="#speakers">{t("accelerate.header.speakers")}</a>
             <a href="#sponsors">{t("accelerate.header.sponsors")}</a>
             <a href="#faq">{t("accelerate.header.faq")}</a>
-            <a href="#">side events</a>
+            <a
+              href="https://lu.ma/solana-nyc?tag=accelerate"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              side events
+            </a>
             <a className={styles.cta} href="#tickets">
               <span>
                 {t("accelerate.header.get-tickets")} <ArrowUpRight />
@@ -115,14 +121,9 @@ const Header = () => {
             {t("accelerate.header.faq")}
           </a>
           <a
-            href="#"
-            onClick={(e) => {
-              e.preventDefault();
-              handleMenu();
-              document
-                .getElementById("faq")
-                ?.scrollIntoView({ behavior: "smooth" });
-            }}
+            href="https://lu.ma/solana-nyc?tag=accelerate"
+            target="_blank"
+            rel="noopener noreferrer"
           >
             Side Events
           </a>
