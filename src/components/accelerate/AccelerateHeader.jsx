215774ab3234851c617f401f1a851a7ae957baca
jon wong <jon.wong@solana.org>
2025-05-13 11:48:49 -0400

    Update src/components/accelerate/AccelerateHeader.jsx

--- src/components/accelerate/AccelerateHeader.jsx
+++ src/components/accelerate/AccelerateHeader.jsx
@@ -43,7 +43,7 @@ const Header = () => {
               <a
                 target="_blank"
                 rel="noopener noreferrer"
-                href="https://lu.ma/solana-accelerate"
+                href="https://lu.ma/solana-accelerate?compact=true"
               >
                 Agenda
               </a>
