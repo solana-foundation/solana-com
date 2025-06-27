395822dda8790619ea4aa1df435887f41bdfc371
harkl <3zy3@protonmail.ch>
2025-06-28 07:59:43 +1200

    bump ecosystem spacing

--- src/pages/[locale]/solutions/depin.tsx
+++ src/pages/[locale]/solutions/depin.tsx
@@ -31,7 +31,7 @@ const DePINPage = () => {
         <WhatIsDepin />
 
         {/* EcoProjects Section */}
-        <section className="py-6">
+        <section className="pt-6 pb-10">
           <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="grid grid-cols-1 md:grid-cols-2 gap-x-8 items-start">
               <h2 className="text-3xl font-bold text-white col-span-full">
