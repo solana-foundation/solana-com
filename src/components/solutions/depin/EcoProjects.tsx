a647353de1bd10e389a5e85273675ca165382c5c
harkl <3zy3@protonmail.ch>
2025-06-28 07:57:40 +1200

    Format carousel

--- src/components/solutions/depin/EcoProjects.tsx
+++ src/components/solutions/depin/EcoProjects.tsx
@@ -169,7 +169,12 @@ export const EcoProjects = () => {
           <div className="pointer-events-none absolute left-0 top-0 h-full w-24 bg-gradient-to-r from-[#090d17] to-transparent z-10" />
           {/* Right Blur */}
           <div className="pointer-events-none absolute right-0 top-0 h-full w-24 bg-gradient-to-l from-[#090d17] to-transparent z-10" />
-          <Carousel panels={6} ref={carouselRef} controlsInline={false} autoPlay={3000}>
+          <Carousel
+            panels={6}
+            ref={carouselRef}
+            controlsInline={false}
+            autoPlay={3000}
+          >
             {items}
           </Carousel>
         </div>
