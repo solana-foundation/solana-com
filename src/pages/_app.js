52fb8f3085a3f77708be02d30e49ad131e263444
John <75003086+ZYJLiu@users.noreply.github.com>
2025-07-31 15:05:13 -0500

    Revert "add inkeep chat component to entire site (#646)" (#654)
    This reverts commit 5caa2f9179dd1c1a09b79bcb87d74b38d588ae86.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,7 +5,6 @@ import { NextIntlClientProvider } from "next-intl";
 import SitewideTopAlert from "../components/sharedPageSections/SitewideTopAlert";
 import GTMTrackingSnippet from "../components/GTMTrackingSnippet";
 import CookieConsent from "@/components/CookieConsent/CookieConsent";
-import { InkeepChatButton } from "@/app/components/inkeep/inkeep-chat-button";
 
 const App = ({ Component, pageProps: { messages, ...pageProps } }) => {
   return (
@@ -18,7 +17,6 @@ const App = ({ Component, pageProps: { messages, ...pageProps } }) => {
       <CookieConsent />
       <ThemeProvider>
         <Component {...pageProps} key={pageProps.key} />
-        <InkeepChatButton />
       </ThemeProvider>
     </NextIntlClientProvider>
   );
