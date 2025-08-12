63ad50f29c667028a5034df80f7bede4bb446992
harkl <3zy3@protonmail.ch>
2025-08-12 15:47:37 +1200

    style elements and footer with lang selector

--- packages/site-chrome/src/index.js
+++ packages/site-chrome/src/index.js
@@ -1,4 +1,2 @@
-export { default as Header } from "./web/Header";
-export { default as Footer } from "./web/Footer";
-// Styles exports (optional for consumers if they need to import)
-export * as ChromeStyles from "./styles";
+export { default as Header } from "./Header";
+export { default as Footer } from "./Footer";
--- packages/site-chrome/src/styles/index.js
+++
@@ -1,3 +0,0 @@
-// re-export style module paths if needed by consumers
-export { default as headerStyles } from "./Header.module.scss";
-export { default as footerStyles } from "./Footer.module.scss";
