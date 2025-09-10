d71f1329921e48b32975f9a31f8ab2561d046c8d
Bram Borggreve <beeman@beeman.nl>
2025-09-10 03:30:16 +0100

    fix: use notFound instead of redirect

--- apps/web/src/app/[locale]/developers/courses/[...rest]/page.tsx
+++ apps/web/src/app/[locale]/developers/courses/[...rest]/page.tsx
@@ -1,5 +1,5 @@
-import { redirect } from "next/navigation";
+import { notFound } from "next/navigation";
 
 export default function RestPage() {
-  return redirect("/developers/courses");
+  return notFound();
 }
