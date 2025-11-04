f0f1c51c1f9c27721b48102beeb9788815476774
guibibeau <g.bibeaulaviolette@gmail.com>
2025-11-04 15:22:29 -0500

    Fix templates rewrite to strip path prefix

--- apps/web/rewrites-redirects.mjs
+++ apps/web/rewrites-redirects.mjs
@@ -15,13 +15,13 @@ export default {
       },
       {
         source: "/templates",
-        destination: "https://solana-com-templates.vercel.app/templates",
+        destination: "https://solana-com-templates.vercel.app/",
         locale: false,
       },
       // everything underneath
       {
         source: "/templates/:path*",
-        destination: "https://solana-com-templates.vercel.app/templates/:path*",
+        destination: "https://solana-com-templates.vercel.app/:path*",
         locale: false,
       },
     ],
