1dc64d924b8323a782f084c6e0664e71c44ea37d
Alena <cgwbmp@gmail.com>
2025-12-05 00:00:58 +0300

    fix: try to connect all stages

--- apps/web/apps-urls.js
+++ apps/web/apps-urls.js
@@ -8,10 +8,11 @@ const vercelMediaUrl = `https://${withRelatedProject({
 const developmentMediaUrl = "http://localhost:3002";
 
 export const MEDIA_APP_URL =
-  process.env.NEXT_PUBLIC_MEDIA_APP_URL ||
-  (process.env.NODE_ENV === "production"
-    ? vercelMediaUrl
-    : developmentMediaUrl);
+  process.env.VERCEL_ENV === "production"
+    ? process.env.NEXT_PUBLIC_MEDIA_APP_URL || vercelMediaUrl
+    : process.env.VERCEL_ENV === "preview"
+      ? vercelMediaUrl
+      : developmentMediaUrl;
 
 // Main app URLs
 const vercelMainUrl = `https://${withRelatedProject({
@@ -21,13 +22,8 @@ const vercelMainUrl = `https://${withRelatedProject({
 const developmentMainUrl = "http://localhost:3000";
 
 export const MAIN_APP_URL =
-  process.env.NEXT_PUBLIC_MAIN_SITE_URL ||
-  (process.env.NODE_ENV === "production" ? vercelMainUrl : developmentMainUrl);
-
-console.log("MAIN_APP_URL", MAIN_APP_URL);
-console.log("MEDIA_APP_URL", MEDIA_APP_URL);
-console.log("process.env.VERCEL_ENV", process.env.VERCEL_ENV);
-console.log(
-  "process.env.VERCEL_RELATED_PROJECTS",
-  process.env.VERCEL_RELATED_PROJECTS,
-);
+  process.env.VERCEL_ENV === "production"
+    ? process.env.NEXT_PUBLIC_MAIN_SITE_URL || vercelMainUrl
+    : process.env.VERCEL_ENV === "preview"
+      ? vercelMainUrl
+      : developmentMainUrl;
