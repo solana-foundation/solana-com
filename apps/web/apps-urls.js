e40bd0e13702c66f9aa1baf6eb44ab639706e2eb
Alena <cgwbmp@gmail.com>
2025-12-05 00:12:20 +0300

    fix: try to connect all stages

--- apps/web/apps-urls.js
+++ apps/web/apps-urls.js
@@ -2,46 +2,24 @@ import { withRelatedProject } from "@vercel/related-projects";
 
 // Media app URLs
 const vercelMediaUrl = `https://${withRelatedProject({
-  projectName: "prj_40kDwTp9P4bvvGlaoU8U0cp4VG6N",
+  projectName: "solana-com-media",
   defaultHost: "solana-com-media.vercel.app",
 })}`;
 const developmentMediaUrl = "http://localhost:3002";
 
 export const MEDIA_APP_URL =
-  process.env.VERCEL_ENV === "production"
-    ? process.env.NEXT_PUBLIC_MEDIA_APP_URL || vercelMediaUrl
-    : process.env.VERCEL_ENV === "preview"
-      ? vercelMediaUrl
-      : developmentMediaUrl;
+  process.env.NEXT_PUBLIC_MEDIA_APP_URL ||
+  (process.env.NODE_ENV === "production"
+    ? vercelMediaUrl
+    : developmentMediaUrl);
 
 // Main app URLs
 const vercelMainUrl = `https://${withRelatedProject({
-  projectName: "prj_sS1OQUbA5luVqKHn2M9yU2r4n4yo",
+  projectName: "solana-com",
   defaultHost: "solana.com",
 })}`;
 const developmentMainUrl = "http://localhost:3000";
 
 export const MAIN_APP_URL =
-  process.env.VERCEL_ENV === "production"
-    ? process.env.NEXT_PUBLIC_MAIN_SITE_URL || vercelMainUrl
-    : process.env.VERCEL_ENV === "preview"
-      ? vercelMainUrl
-      : developmentMainUrl;
-
-console.log("MAIN_APP_URL", MAIN_APP_URL);
-console.log("MEDIA_APP_URL", MEDIA_APP_URL);
-console.log("vercelMediaUrl", vercelMediaUrl);
-console.log("vercelMainUrl", vercelMainUrl);
-console.log("process.env.VERCEL_ENV", process.env.VERCEL_ENV);
-console.log(
-  "process.env.VERCEL_RELATED_PROJECTS",
-  process.env.VERCEL_RELATED_PROJECTS,
-);
-console.log(
-  "process.env.NEXT_PUBLIC_MAIN_SITE_URL",
-  process.env.NEXT_PUBLIC_MAIN_SITE_URL,
-);
-console.log(
-  "process.env.NEXT_PUBLIC_MEDIA_APP_URL",
-  process.env.NEXT_PUBLIC_MEDIA_APP_URL,
-);
+  process.env.NEXT_PUBLIC_MAIN_SITE_URL ||
+  (process.env.NODE_ENV === "production" ? vercelMainUrl : developmentMainUrl);
