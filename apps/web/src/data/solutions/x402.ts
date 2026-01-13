68a5c029d5fd9af9e8945767aa153d072bc97e26
adlonymous <adlonymous@gmail.com>
2026-01-12 20:14:19 -0500

    updated x402 pages

--- apps/web/src/data/solutions/x402.ts
+++ apps/web/src/data/solutions/x402.ts
@@ -1,5 +1,9 @@
 export const PRODUCTS = [
   {
+    key: "x402",
+    href: "https://x402.org/",
+  },
+  {
     key: "corbits",
     href: "https://corbits.dev/",
   },
@@ -12,26 +16,42 @@ export const PRODUCTS = [
     href: "https://www.x402scan.com/",
   },
   {
-    key: "crossmint",
-    href: "https://www.crossmint.com/solutions/ai-agents",
+    key: "t54",
+    href: "https://t54.ai",
+  },
+  {
+    key: "cdp",
+    href: "https://docs.cdp.coinbase.com/embedded-wallets/welcome",
   },
 ];
 
 export const TOOLS = [
   {
     key: "0",
-    href: "https://docs.corbits.dev/quickstart",
+    href: "https://x402.gitbook.io/x402",
   },
   {
     key: "1",
-    href: "https://docs.payai.network/x402/servers/introduction",
+    href: "https://docs.corbits.dev/quickstart",
   },
   {
     key: "2",
-    href: "https://templates.solana.com/x402-template",
+    href: "https://docs.payai.network/x402/servers/introduction",
   },
   {
     key: "3",
-    href: "https://solana.com/developers/templates/gill-node-solanax402",
+    href: "https://www.t54.ai/x402-secure",
+  },
+  {
+    key: "4",
+    href: "https://x402.gitbook.io/x402/guides/mcp-server-with-x402",
+  },
+  {
+    key: "5",
+    href: "https://x402.gitbook.io/x402/guides/mcp-server-with-x402",
+  },
+  {
+    key: "6",
+    href: "https://solana.com/developers/templates",
   },
 ];
