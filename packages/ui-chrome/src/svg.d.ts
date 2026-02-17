07eace9667e52b1126df96cac37bbd4a84c0624d
Alena <31825666+cgwbmp@users.noreply.github.com>
2026-02-17 04:09:47 +0300

    feat: skinning the inkeep widget (#1125)
    * feat: skinning the inkeep widget
    
    * fix: move api key to env and update styles

--- packages/ui-chrome/src/svg.d.ts
+++ packages/ui-chrome/src/svg.d.ts
@@ -3,3 +3,8 @@ declare module "*.inline.svg" {
   const content: FC<SVGProps<SVGElement>>;
   export default content;
 }
+
+declare module "*.svg" {
+  const content: string;
+  export default content;
+}
