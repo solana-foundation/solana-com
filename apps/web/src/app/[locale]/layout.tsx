4cb16870e687948dd34953435fb85e6ed2e99c2f
hårkl <89045610+h4rkl@users.noreply.github.com>
2026-02-28 01:36:25 +0200

    fix: sentry top 3 errors (#1201)
    * fix: extend error handling for extension filenames in instrumentation client
    
    * fix: https://solana-fndn.sentry.io/issues/6808814113/?project=4509482196140032&query=is%3Aunresolved&referrer=issue-stream

--- apps/web/src/app/[locale]/layout.tsx
+++ apps/web/src/app/[locale]/layout.tsx
@@ -8,9 +8,10 @@ import { Metadata } from "next";
 import { PostHogProvider } from "@@/src/app/components/posthog/PostHogProvider";
 import { config } from "@@/src/config";
 import { getBaseMetadata } from "@@/src/app/metadata";
-import { staticLocales } from "@workspace/i18n/config";
+import { locales, staticLocales } from "@workspace/i18n/config";
 import { getLangDir } from "rtl-detect";
 import { headers } from "next/headers";
+import { notFound } from "next/navigation";
 import {
   Header,
   Footer,
@@ -27,6 +28,11 @@ type Props = {
 
 export default async function RootLayout({ children, params }: Props) {
   const { locale = "en" } = await params;
+
+  if (!locales.includes(locale)) {
+    notFound();
+  }
+
   const direction = getLangDir(locale);
   // Load messages directly
   const messages = (await import(`@@/public/locales/${locale}/common.json`))
