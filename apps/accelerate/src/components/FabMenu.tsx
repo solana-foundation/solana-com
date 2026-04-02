7dfa0984cc2a24718d6ca8106382f5575903e0d6
harkl <3zy3@protonmail.ch>
2026-04-02 14:25:15 +1300

    refactor: update import path for SolanaFabMenu in FabMenu components
    - Changed the import statement for SolanaFabMenu in both accelerate and breakpoint FabMenu components to streamline the import from "@solana-com/fab-menu" instead of "@solana-com/fab-menu/react".
    - Updated the index export in fab-menu package to include SolanaFabMenu from the new react-wrapper for better modularity.

--- apps/accelerate/src/components/FabMenu.tsx
+++ apps/accelerate/src/components/FabMenu.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { SolanaFabMenu } from "@solana-com/fab-menu/react";
+import { SolanaFabMenu } from "@solana-com/fab-menu";
 
 export function FabMenu() {
   return <SolanaFabMenu position="bottom-right" logoVariant="color" />;
